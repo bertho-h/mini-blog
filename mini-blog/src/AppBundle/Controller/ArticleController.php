8a5e7ea7c268e0c647b8dd3d4471ff0ff9549bff
bertho-h <hugoberthon@hotmail.fr>
2018-02-22 20:03:31 +0100

    added delete link aside each article, worked a little bit the design

--- mini-blog/src/AppBundle/Controller/ArticleController.php
+++ mini-blog/src/AppBundle/Controller/ArticleController.php
@@ -38,14 +38,14 @@ class ArticleController extends Controller {
   }
 
   /**
-*@Route("/articles/view/{id}", name="article.view")
-*@Method({"GET", "POST"})
-*/
-public function viewAction(Request $request, Article $article) {
-  return $this->render('articles/view.html.twig', [
-    'article' => $article
-  ]);
-}
+  *@Route("/articles/view/{id}", name="article.view")
+  *@Method({"GET", "POST"})
+  */
+  public function viewAction(Request $request, Article $article) {
+    return $this->render('articles/view.html.twig', [
+      'article' => $article
+    ]);
+  }
 
 
   /**
@@ -66,6 +66,17 @@ public function viewAction(Request $request, Article $article) {
       'form' => $form->createView(),
       'article' => $article,
     ]);
+  }
+
+  /**
+  *@Route("/articles/delete/{id}", name="article.delete")
+  *@Method({"GET","POST"})
+  */
+  public function deleteAction(Request $request, Article $article) {
+    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager->remove($article);
+    $entityManager->flush();
+    return $this->redirectToRoute('article.index');
   }
 
 }
