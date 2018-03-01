45dffa0719ac0958e12feeb7c57459f1a7e37cab
bertho-h <hugoberthon@hotmail.fr>
2018-03-01 13:18:29 +0100

    added signup link to login page and signup route with form

--- mini-blog/src/AppBundle/Controller/SecurityController.php
+++ mini-blog/src/AppBundle/Controller/SecurityController.php
@@ -4,7 +4,11 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SecurityController extends Controller {
 
@@ -30,6 +34,33 @@ class SecurityController extends Controller {
       'username' => $lastUsername,
       'error'=> $error
     ]);
+  }
+
+  /**
+  *@Route("/signup", name="signup")
+  *
+  */
+  public function signupAction(Request $request) {
+    $user = new User;
+    $em = $this->getDoctrine()->getManager();
+    $form = $this->createForm(UserType::class, $user);
+    $encoder = $this->get('security.password_encoder');
+
+    $form->add('Create', SubmitType::class);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $user->setUsername($form['username']->getData());
+      $password = $encoder->encodePassword($user, $form['username']->getData());
+      $user->setPassword($password);
+      $em->persist($user);
+      $em->flush();
+      return $this->redirectToRoute('login');
+    }
+    return $this->render('security/signup.html.twig', [
+      'form' => $form->createView()
+    ]);
+
   }
 }
 
